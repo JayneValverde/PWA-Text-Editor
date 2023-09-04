fcc8e34db601a4af697721c9a872c46d487ce121
Jayne Valverde <jaynevalverde@gmail.com>
2023-09-04 17:05:11 -0500

    database-PUT

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,8 +12,23 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  console.error('PUT to the database');
+
+  // Create a connection to the database and version we want to use
+  const contactDb = await openDB('jate', 1);
+
+  // Create new transaction and specify the database & data privileges
+  const tx = contactDb.transaction('jate', 'readwrite');
+
+  // Open the desired object
+  const request = store.put({ id: 1, value: content });
+
+  // Get confirmation of the request
+  const result = await request; 
+  console.log('🚀 - data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
