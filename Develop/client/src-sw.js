e4eb6b127145e7c4fe9843f9331a0e84beabff90
Jayne Valverde <jaynevalverde@gmail.com>
2023-09-04 16:49:46 -0500

    webpack config

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -27,4 +27,21 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  // define the callback function that will filter the requests we want to cache (in this case, JS & CSS files)
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    // name of the cache storage
+    cacheName: 'asset-cache',
+    plugins: [
+      // this will cache responses with the headers to a maximum-age of 30 days
+      new CacheableResponsePlugin({
+        statuses: [0,200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 60, 
+        maxAgeSeconds: 30 * 24 * 60, // 30 days
+      })
+    ],
+  })
+);
